7ce0dc3ba03dca0a6d790dbe9faa27b86aa4fbc4
shanbyjucode <69505225+shanbyjucode@users.noreply.github.com>
2022-01-18 22:52:52 +0530

    Add files via upload

---
+++ precache-manifest.1cb673890bfc57bcd3493d42d1e316b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd38ccf3b4725d13fe5e5f3f6bdb91ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef9743dfc82c69481b54",
+    "url": "/static/css/main.60c40d9f.chunk.css"
+  },
+  {
+    "revision": "bb131cedf02f88e95754",
+    "url": "/static/js/2.7af21a55.chunk.js"
+  },
+  {
+    "revision": "4651a60dd24a3d76949974fa9e435183",
+    "url": "/static/js/2.7af21a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef9743dfc82c69481b54",
+    "url": "/static/js/main.f7aa7d4e.chunk.js"
+  },
+  {
+    "revision": "54ff6f61acf9b1d227fb",
+    "url": "/static/js/runtime-main.35769ded.js"
+  }
+]);
