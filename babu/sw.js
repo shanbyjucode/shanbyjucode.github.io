2d3cae25563792ebefb558a41c9efb48c927f69b
shanbyjucode <69505225+shanbyjucode@users.noreply.github.com>
2025-07-21 12:55:06 +0530

    Add files via upload

---
+++ babu/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'chicken-calc-cache-v2'; // Change version to bust old cache
+
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  // Add other assets if any (images, icons)
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
